6e34ac35df233e497e9fdce85bae637342bd1929
hzrd149 <github@hzrd149.com>
2025-10-11 18:52:17 +0100

    small fix

--- src/componnts/UserProfile.tsx
+++ src/componnts/UserProfile.tsx
@@ -1,7 +1,5 @@
 import UserAvatar from "./UserAvatar";
 import UserName from "./UserName";
-import { npubEncode } from "nostr-tools/nip19";
-import { useSearch } from "../context/SearchContext";
 
 interface UserProfileProps {
   pubkey: string;
@@ -14,22 +12,6 @@ interface UserProfileProps {
 }
 
 export default function UserProfile(props: UserProfileProps) {
-  const { updateSearch } = useSearch();
-
-  const handleUsernameClick = () => {
-    const npub = npubEncode(props.pubkey);
-    const currentQuery =
-      new URLSearchParams(window.location.search).get("q") || "";
-    const trimmedQuery = currentQuery.trim();
-    const newTerm = `by:${npub}`;
-
-    if (!trimmedQuery) {
-      updateSearch(newTerm);
-    } else if (!trimmedQuery.includes(newTerm)) {
-      updateSearch(`${trimmedQuery} ${newTerm}`);
-    }
-  };
-
   return (
     <div class={`flex items-center gap-3 ${props.class || ""}`}>
       <UserAvatar
@@ -42,7 +24,6 @@ export default function UserProfile(props: UserProfileProps) {
         showPubkey={props.showPubkey}
         maxLength={props.maxNameLength}
         class={props.nameClass}
-        onClick={handleUsernameClick}
       />
     </div>
   );
